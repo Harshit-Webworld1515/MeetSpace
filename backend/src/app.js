86c3376bf87231a93f69e81963a8eb42c8c84928
Harshit <harshitpandit3525@gmail.com>
2026-09-06 10:34:20 +0530

    Setup Express server with Socket.IO and MongoDB

---
+++ backend/src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+import cors from "cors";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+const server = createServer(app);
+//creating the socket server by passing the http server instance to it
+// const io = new Server(server);
+const io = connectToSocket(server);
+
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({ extended: true, limit: "40kb" }));
+
+app.set("port", process.env.PORT || 8080);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const startServer = async () => {
+  const connectionDb = await mongoose.connect("mongodb+srv://nationfirst1515_db_user:dGYJpIySHE4vRkhs@cluster0.amscjhw.mongodb.net");
+  console.log(`Connected to MongoDB Atlas Host: ${connectionDb.connection.host}`);
+
+  server.listen(app.get("port"), () => {
+    console.log(`Server is running on port ${app.get("port")}`);
+  });
+};
+
+startServer();
---
+++ backend/src/routes/users.route.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router =Router();
+router.route("/register"); 
+router.route("/login");
+router.route("/add_to_activity");
+router.route("/get_all_activity");
+
+export default router;
