6a1df50244ed3a2254d8661c13cb74c424462e53
Harshit <harshitpandit3525@gmail.com>
2026-09-14 11:06:50 +0530

    Creating our interactive web page of meetspace

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import '../app.css'
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
   return (
-    <div>Landing page</div>
+    <div className='landingPageContainer'>
+      <nav>
+        <div className='navHeader'>
+          <h2>MeetSpace</h2>
+        </div>
+        <div className='navlist'>
+          <p>join as guest</p>
+          <p>Register</p>
+          <div role='button'>
+            <p>login</p>
+          </div>
+        </div>
+      </nav>
+      <div className="landingMainContainer">
+        <div>
+          <h1><span style={{ color: "#ff9839" }}>Connect</span> with your Loved ones</h1>
+          <p>
+            Join our community and connect with your loved ones, no matter the distance, through MeetSpace.
+          </p>
+          <div role='button' >
+            <Link to={"/home"}>Get Started</Link>
+          </div>
+        </div>
+        <div>
+          <img src="/mobile.png" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 //css to jsx
