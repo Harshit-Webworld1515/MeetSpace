e8f8e50c12e568920fe9cde2e6796181993c885e
Harshit <harshitpandit3525@gmail.com>
2026-09-16 00:00:00 +0530

    feat: configure app routes with AuthProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/landing'
-import {Authentication} from './pages/authentication'
+import { Authentication } from './pages/authentication'
 import './App.css'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
   return (
     <>
-    <Router>
-
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/auth' element={<Authentication />} />
-      </Routes>
-
-    </Router>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/auth' element={<Authentication />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
     </>
   )
 }
